2f0cccc7876513d4f9ae9fd9f0e92a0303abecc5
Saad Ali Khan <saad.khan5891@gmail.com>
2020-04-03 23:26:14 +0500

    commit

--- src/login.js
+++ src/login.js
@@ -26,39 +26,83 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Container from '@material-ui/core/Container';
 
-class Login extends React.Component {
 
-  
-  render() {
 
-    const useStyles = makeStyles((theme) => ({
-      paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      },
-      avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-      },
-      form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-      },
-      submit: {
-        margin: theme.spacing(3, 0, 2),
-      },
-    }));
-  
-    return (
-      <Container component="login" maxWidth="xs">
+function Copyright() {
+
+  return (
+
+    <Typography variant="body2" color="textSecondary" align="center">
+
+      {'Copyright © '}
+
+      <Link color="inherit" href="https://material-ui.com/">
+
+        Your Website
+
+      </Link>{' '}
+
+      {new Date().getFullYear()}
+
+      {'.'}
+
+    </Typography>
+
+  );
+}
+const useStyles = makeStyles((theme) => ({
+
+  paper: {
+
+    marginTop: theme.spacing(8),
+
+    display: 'flex',
+
+    flexDirection: 'column',
+
+    alignItems: 'center',
+
+  },
+
+  avatar: {
+
+    margin: theme.spacing(1),
+
+    backgroundColor: theme.palette.secondary.main,
+
+  },
+
+  form: {
+
+    width: '100%', // Fix IE 11 issue.
+
+    marginTop: theme.spacing(1),
+
+  },
+
+  submit: {
+
+    margin: theme.spacing(3, 0, 2),
+
+  },
+
+}));
+
+
+
+export default function SignIn() {
+
+  const classes = useStyles();
+
+  return (
+
+    <Container component="main" maxWidth="xs">
 
       <CssBaseline />
 
-      <div className={useStyles.paper}>
+      <div className={classes.paper}>
 
-        <Avatar className={useStyles.avatar}>
+        <Avatar className={classes.avatar}>
 
           <LockOutlinedIcon />
 
@@ -70,7 +114,7 @@ class Login extends React.Component {
 
         </Typography>
 
-        <form className={useStyles.form} noValidate>
+        <form className={classes.form} noValidate>
 
           <TextField
 
@@ -134,7 +178,7 @@ class Login extends React.Component {
 
             color="primary"
 
-            className={useStyles.submit}
+            className={classes.submit}
 
           >
 
@@ -170,10 +214,13 @@ class Login extends React.Component {
 
       </div>
 
+      <Box mt={8}>
+
+        <Copyright />
+
+      </Box>
+
     </Container>
-    )
-  }
+
+  );
 }
-
-export default Login;
-
